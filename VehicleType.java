f5b92445bf4bda0a1e528d1ae29e17227e2cd7d4
Priti More <168186487+Pritimore1104@users.noreply.github.com>
2025-04-29 15:00:54 +0530

    Update VehicleType.java

--- VehicleType.java
+++ VehicleType.java
@@ -1,4 +1,17 @@
-// VehicleTypeJpaRepository.java
-public interface VehicleTypeJpaRepository extends JpaRepository<VehicleType, Long> {
-    List<VehicleType> findByWheels(int wheels);
+package com.example.vehiclebooking.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class VehicleType {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int wheels;
+
+    public VehicleType() {}
+    public VehicleType(String name, int wheels) {
+        this.name = name;
+        this.wheels = wheels;
+    }
 }
