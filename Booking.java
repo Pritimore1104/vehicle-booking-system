407cf21105447496b132282ababbb01ea8b26278
Priti More <168186487+Pritimore1104@users.noreply.github.com>
2025-04-29 15:23:55 +0530

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,6 +1,19 @@
-// BookingJpaRepository.java
-public interface BookingJpaRepository extends JpaRepository<Booking, Long> {
-    boolean existsByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
-        Vehicle vehicle, LocalDate endDate, LocalDate startDate
-    );
+package com.example.vehiclebooking.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Booking {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    private Vehicle vehicle;
+
+    public Booking() {}
 }
